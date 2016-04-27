9eb8f55b5899513ec45c9d7f39f222aaa7c1490d
Cedric Kisema <cedric.kisema@gmail.com>
2016-04-27 16:41:31 +0100

    introduce play game with main method

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject tic-tac-toe "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Tic Tac Toe"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-RC2"]]
+  :main ^:skip-aot tic-tac-toe.play-game
   :profiles {:dev {:dependencies [[speclj "3.3.1"]]}}
   :plugins [[speclj "3.3.1"]]
   :test-paths ["spec"])
---
+++ src/tic_tac_toe/play_game.clj
@@ -0,0 +1,7 @@
+(ns tic-tac-toe.play-game
+  (:gen-class))
+
+(defn -main
+  [& args]
+  (println "Welcome to Tic Tac Toe\n")
+  (println "---\n---\n---\n"))
