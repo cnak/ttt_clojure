140ef158c004adcbe72aa63bbac4b6652462cd02
Cedric Kisema <cedric.kisema@gmail.com>
2016-05-03 10:50:04 +0100

    refactored board

--- src/tic_tac_toe/board.clj
+++ src/tic_tac_toe/board.clj
@@ -1,13 +1,11 @@
 (ns tic-tac-toe.board)
 
 (defn- valid-location? [location board] 
-  (if (<= location (- (count board) 1))
-     true
-     false))
+  (<= location (- (count board) 1)))
 
 (defn make-move [board location mark]
   (if (valid-location? location board)
     (assoc board location mark) board))
 
-(defn empty-board [] ["-" "-" "-" "-" "-" "-" "-" "-" "-"])
+(defn empty-board [] (repeat 9 "-"))
 
