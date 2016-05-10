3bcec9eb56f1a7ac92f141caea2c0ca27e82d515
Cedric Kisema <cedric.kisema@gmail.com>
2016-05-10 15:36:34 +0100

    refactored winning method

--- src/tic_tac_toe/board.clj
+++ src/tic_tac_toe/board.clj
@@ -16,29 +16,22 @@
     false
     (apply = (row board))))
 
-(defn- top-row-the-winner? [board]
-  (cells-the-same? #(get-cells % [0 1 2])board))
+(def winning-positions [[0 1 2] 
+                        [3 4 5] 
+                        [6 7 8]
+                        [1 4 7]
+                        [2 5 8]
+                        [0 3 6]
+                        [0 4 8]
+                        [2 4 6]])
 
-(defn- middle-row-the-winner? [board]
-  (cells-the-same? #(get-cells % [3 4 5])board))
+(defn- all-cell-same? [board line] (cells-the-same? #(get-cells % line) board)) 
 
-(defn- bottom-row-the-winner? [board]
-  (cells-the-same? #(get-cells % [6 7 8])board))
-
-(defn- left-column-winner? [board]
-  (cells-the-same? #(get-cells % [0 3 6])board))
-
-(defn- middle-column-winner? [board]
-  (cells-the-same? #(get-cells % [1 4 7])board)) 
-
-(defn- right-column-winner? [board]
-  (cells-the-same? #(get-cells % [2 5 8])board)) 
-
-(defn- diagonal-top-left-winner? [board]
-  (cells-the-same? #(get-cells % [0 4 8])board))
-
-(defn- diagonal-top-right-winner? [board]
-  (cells-the-same? #(get-cells % [2 4 6])board))
+(defn winner? [board]
+  (loop [my-board board position-size (count winning-positions) result false]
+    (if (= result true) result
+    (if (= position-size 0) result
+      (recur my-board (dec position-size) (all-cell-same? my-board (nth winning-positions (- position-size 1))))))))
 
 (defn- board-empty? [board]
   (every? #{empty-mark} board))
@@ -56,10 +49,7 @@
 (defn empty-board [] (repeat 9 empty-mark))
 
 (defn game-won? [board]
-  (or (top-row-the-winner? board) (middle-row-the-winner? board) (bottom-row-the-winner? board) 
-      (left-column-winner? board) (middle-column-winner? board) (right-column-winner? board) 
-      (diagonal-top-left-winner? board)
-      (diagonal-top-right-winner? board)))
+  (winner? board))
 
 (defn game-drawn? [board]
   (if (game-won? board) false
@@ -80,5 +70,5 @@
 (defn winner [board]
   (if (game-drawn? board)
     "-"
-  (if (= (current-player board) "O") "X" "O")))
+    (if (= (current-player board) "O") "X" "O")))
 
