0251b34bf9a2318fc63b9d3ea8da8daed8ff3048
Cedric Kisema <cedric.kisema@gmail.com>
2016-05-16 14:23:29 +0100

    calculates score for win doraw lost games

--- spec/tic_tac_toe/computer_player_spec.clj
+++ spec/tic_tac_toe/computer_player_spec.clj
@@ -1,7 +1,9 @@
 (ns tic-tac-toe.computer-player-spec
   (:require [speclj.core :refer :all]
             [tic-tac-toe.computer-player :refer :all]
-            [tic-tac-toe.player :as player]))
+            [tic-tac-toe.player :as player]
+            [tic-tac-toe.board :as board]
+            ))
 
 (defn get-computer-move [board]
   (player/get-move {:type-of-player :computer} board))
@@ -16,6 +18,27 @@
     (should= 1 (get-computer-move ["X" "-" "-"
                                    "-" "-" "-"
                                    "-" "-" "-"
-                                   ])))
-  )
+                                   ]))))
+(def winning-board
+  ["O" "X" "X" 
+   "O" "O" "X"
+   "-" "-" "X"])
+
+(def drawn-board
+  ["O" "X" "O" 
+   "O" "O" "X"
+   "X" "O" "X"])
+
+(def lost-board
+  ["O" "X" "X" 
+   "X" "O" "-"
+   "-" "-" "O"])
+
+(describe "score"
+  (it "scores 10 for a winning move"
+    (should= 10 (score winning-board "X")))
+  (it "scores 0 for a draw game"
+    (should= 0 (score drawn-board "X")))
+  (it "scores -10 for a lost game for player X"
+    (should= -10 (score lost-board "X"))))
 
--- src/tic_tac_toe/computer_player.clj
+++ src/tic_tac_toe/computer_player.clj
@@ -5,5 +5,10 @@
 (defn- move [board]
   (first (board-logic/remaining-moves board)))
 
+(defn score [board player]
+  (let [winner (board-logic/winner board)]
+    (if (= winner player) 10
+      (if (board-logic/game-drawn? board) 0 -10))))
+
 (defmethod player/get-move :computer [_ board]
   (move board))
