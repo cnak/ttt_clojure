3415173a6fdfbbd48eb4405b987d38411a070563
Cedric Kisema <cedric.kisema@gmail.com>
2016-06-01 15:20:43 +0100

    clean up

--- spec/tic_tac_toe/computer_player_spec.clj
+++ spec/tic_tac_toe/computer_player_spec.clj
@@ -7,43 +7,25 @@
 (defn get-computer-move [board]
   (player/get-move {:type-of-player :computer} board))
 
-(def second-move-board
-   ["X" "-" "-"
-    "-" "-" "-"
-    "-" "-" "-"])
-
 (def block-move-board
    ["X" "X" "O"
     "-" "O" "-"
     "X" "-" "-"] )
 
-(def win-board2
+(def win-board
    ["X" "X" "-"
     "O" "O" "-"
     "X" "O" "X"])
 
-(def winning-board
-   ["X" "X" "-"
-    "O" "O" "X"
-    "X" "O" "O"] )
+(def center-move-board
+   ["X" "-" "-"
+    "-" "-" "-"
+    "-" "-" "-"])
 
-(def drawn
-   ["X" "X" "-"
-    "O" "O" "-"
-    "X" "O" "X"])
-
-(describe "best move"
+(describe "ai picks best move"
   (it "picks winning move"
-    (should= 2 (get-computer-move winning-board)))
-  (it "picks winning move"
-    (should= 5 (get-computer-move win-board2)))
+    (should= 5 (get-computer-move win-board)))
   (it "blocks move 3"
     (should= 3 (get-computer-move block-move-board)))
   (it "returns move in the center"
-    (should= 4 (get-computer-move second-move-board)))) 
-
-(describe "get best"
-  (it "returns best tuple for maximazing player"
-    (should= [2 100] (get-best-results [[2 100]] true)))
-  (it "returns worst tuple for minimizing player"
-        (should= [1 -100] (get-best-results [[1 -100] [2 100]] false))))
+    (should= 4 (get-computer-move center-move-board)))) 
--- src/tic_tac_toe/computer_player.clj
+++ src/tic_tac_toe/computer_player.clj
@@ -1,34 +1,30 @@
 (ns tic-tac-toe.computer-player
   (:require [tic-tac-toe.player :as player]
-            [tic-tac-toe.board :as board-logic]))
-
-(defn- move [board]
-  (first (board-logic/remaining-moves board)))
+            [tic-tac-toe.board :as logic]))
 
 (defn- switch-player [mark]
   (if (= mark "X") "O" "X"))
 
-(defn score [board player depth move real]
-  (let [winner (board-logic/winner board)]
+(defn- score [board depth player]
+  (let [winner (logic/winner board)]
     (cond 
-      (= winner real) [-1 (- 100 depth)] 
+      (= winner player) [-1 (- 100 depth)] 
       (= winner "-") [-1 0]
-      (= winner (switch-player real)) [-1 (- -100 depth)])))
+      (= winner (switch-player player)) [-1 (- -100 depth)])))
 
-(defn get-best-results [moves-scores maximize-player?]
-    (if maximize-player?  
-       (first (sort-by second > moves-scores))
-       (first (sort-by second < moves-scores))))
+(defn- get-best-results [moves-scores maximize-player?]
+  (if maximize-player?  
+    (first (sort-by second > moves-scores))
+    (first (sort-by second < moves-scores))))
 
-(defn minimax [board current-player depth move perspective real]
-  (if (board-logic/game-over? board)
-      (score board current-player depth move real)
-  (let [possible-moves   (board-logic/remaining-moves board)
-        results           (map #(minimax (board-logic/make-move board %1 (board-logic/current-player board)) (switch-player current-player) (inc depth) %1 (not perspective) real) possible-moves)
-        scores (map second results)
-        move-scores (zipmap possible-moves scores)
-        ]
-    (get-best-results move-scores perspective))))
+(defn- minimax [board current-player depth max-player? perspective-player]
+  (if (logic/game-over? board)
+    (score board depth perspective-player)
+    (let [possible-moves   (logic/remaining-moves board)
+          results           (map #(minimax (logic/make-move board %1 (logic/current-player board)) (switch-player current-player) (inc depth) (not max-player?) perspective-player) possible-moves)
+          scores (map second results)
+          move-scores (zipmap possible-moves scores)]
+      (get-best-results move-scores max-player?))))
 
 (defmethod player/get-move :computer [_ board]
-  (first (minimax board (board-logic/current-player board) 0 -1 true (board-logic/current-player board))))
+  (first (minimax board (logic/current-player board) 0 true (logic/current-player board))))
